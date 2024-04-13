0191b4de3ae609240304d3d2f6edfa8f433e3564
Nitesh1029 <niteshkumar145870@gmail.com>
2024-04-13 15:54:39 +0530

    route issue fixed

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
           <Route exact path="/about">
             <About mode={mode}/>
           </Route>
-          <Route exact path="/">
+          <Route path="/">
             <Textbar showAlert={showAlert} heading="WordEditor - Word Counter, Character Counter, Extra Space Remover" mode={mode}/>
           </Route>
       </Switch>
